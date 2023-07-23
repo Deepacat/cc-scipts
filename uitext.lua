2bf705bb49b6b1a09fe922c05dcc8acd8098e323
Zernaie <evanaterx@gmail.com>
2023-07-23 02:09:32 -0500

    test

--- uitext.lua
+++ uitext.lua
@@ -1,3 +1,8 @@
+for i = 1, 10 do
+  for i = 1, 10 do
+    print( "#")
+end
+
 while true do
   local event, button, x, y = os.pullEvent( "mouse_click" )
   print( "The mouse button ", button, " was pressed at ", x, " and ", y )
